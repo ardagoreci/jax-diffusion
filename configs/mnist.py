b8d46f6eacc936d560fe8a8f04ded35be87481dd
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2022-08-15 22:04:47 +0100

    Improved configs

--- configs/mnist.py
+++ configs/mnist.py
@@ -2,26 +2,21 @@
 Hyperparameter configuration file for MNIST.
 MNIST will be used for testing the functionality of the framework.
 """
-import ml_collections
+import default as default_config
 
 
 def get_config():
     """Get the hyperparameter configuration for training on MNIST"""
-    config = ml_collections.ConfigDict()
-    config.model = 'UNetModel'
+    config = default_config.get_config()
     config.dataset = 'mnist'
+    config.batch_size = 128
 
-    config.learning_rate = 0.1
-    config.batch_size = 256
-    config.warmup_epochs = 5.0
-    config.momentum = 0.9
+    # Model hyperparameters
+    config.model_channels = 4
+    config.in_channels = 1
+    config.out_channels = 1
+    config.num_res_blocks = 2
+    config.attention_resolutions = ()
+    config.channel_mult = (1, 2, 4, 8)
 
-    config.num_epochs = 100
-    config.log_every_n_steps = 100
-
-    config.cache = False
-    config.half_precision = False
-
-    config.num_train_steps = -1
-    config.steps_per_eval = -1
     return config
