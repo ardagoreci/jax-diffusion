389a722d4c16c082a9ff8a1b76d929e442c612ea
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2022-08-15 23:09:54 +0100

    timesteps added to the call of p_train_step

--- train.py
+++ train.py
@@ -244,7 +244,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict,
     train_metrics_last_t = time.time()
     logging.info("Initial compilation, this might take some minutes...")
     for step, batch in zip(range(step_offset, num_steps), train_iter):
-        state, metrics = p_train_step(state, batch, jnp.arange(0, config.local_batch_size))
+        state, metrics = p_train_step(state, batch, jnp.arange(0, config.batch_size))
         for h in hooks:
             h(step)
         if step == step_offset:
