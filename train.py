c9c8e39ea42cf6447f725ac872a2bf2a686a4b80
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2022-08-16 15:19:43 +0100

    Fixed simple bug

--- train.py
+++ train.py
@@ -133,7 +133,7 @@ def create_input_iter(name: str,
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     iterator = map(prepare_tf_data, dataset)
     # iterator = input_pipeline.batch_iterator(iterator)
-    iterator = jax.tree_util.tree_map(lambda x: input_pipeline.Batch(x[0], x[1]))
+    iterator = jax.tree_util.tree_map(lambda x: input_pipeline.Batch(x[0], x[1]), iterator)
     iterator = flax.jax_utils.prefetch_to_device(iterator, size=2)
     return iterator
 
