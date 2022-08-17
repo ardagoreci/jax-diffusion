c962e8b82d8c044bd8bd7fab5f83489f8a42dfc6
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2022-08-17 21:43:56 +0100

    Changed training configs

--- configs/celeb_a.py
+++ configs/celeb_a.py
@@ -15,7 +15,7 @@ def get_config():
 
     # Training hyperparameters
     config.steps_per_epoch = 200
-    config.steps_per_eval = 20
+    config.steps_per_eval = 20  # 20 steps per eval
     config.steps_per_checkpoint = 200  # save a checkpoint every epoch
     config.num_steps = 500_000  # for CelebA HQ, 500_000 steps were used
     # config.steps_per_eval = 100
